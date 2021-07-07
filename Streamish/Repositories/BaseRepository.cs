9e38d6013b6baed6d6e7394fcf63f222b8028c69
NSPatton <greengun15@gmail.com>
2021-07-07 16:01:34 -0500

    fixed my sql command for getbyid

--- Streamish/Repositories/BaseRepository.cs
+++ Streamish/Repositories/BaseRepository.cs
@@ -1,11 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace Streamish.Repositories
 {
-    public class BaseRepository
+    public abstract class BaseRepository
     {
+        private readonly string _connectionString;
+
+        public BaseRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_connectionString);
+            }
+        }
     }
 }
+
