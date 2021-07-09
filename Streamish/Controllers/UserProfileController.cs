d230aa409e77288989b485313a8146633364a7b6
NSPatton <greengun15@gmail.com>
2021-07-09 14:00:28 -0500

    Added full CRUD for UserProfile

---
+++ Streamish/Controllers/UserProfileController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Streamish.Repositories;
+using Streamish.Models;
+
+namespace Streamish.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly IUserProfileRepository _userRepo;
+        public UserProfileController(IUserProfileRepository userProfileRepository)
+        {
+            _userRepo = userProfileRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_userRepo.GetAllUserProfiles());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var userProfile = _userRepo.GetUserProfileById(id);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+            return Ok(userProfile);
+        }
+
+        [HttpPost]
+        public IActionResult Post(UserProfile userProfile)
+        {
+            _userRepo.Add(userProfile);
+            return CreatedAtAction("Get", new { id = userProfile.Id }, userProfile);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, UserProfile userProfile)
+        {
+            if (id != userProfile.Id)
+            {
+                return BadRequest();
+            }
+
+            _userRepo.Update(userProfile);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _userRepo.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Streamish/Repositories/IUserProfileRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Streamish.Models;
+
+namespace Streamish.Repositories
+{
+    public interface IUserProfileRepository
+    {
+        void Add(UserProfile userProfile);
+        void Delete(int id);
+        List<UserProfile> GetAllUserProfiles();
+        UserProfile GetUserProfileById(int id);
+        void Update(UserProfile userProfile);
+    }
+}
